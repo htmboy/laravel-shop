c62738256349d5d9c0943365196e51e1d9c69a83
htmboy <htmboy8@msn.com>
2020-05-07 23:52:15 +0800

    用户界面-收藏商品列表

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,11 @@
 */
 
 
-Route::get('/', 'PagesController@root')->name('root');
+//Route::get('/', 'PagesController@root')->name('root');
+Route::redirect('/', '/products')->name('root');
+Route::get('products', 'ProductsController@index')->name('products.index');
+Route::get('products', 'ProductsController@index')->name('products.index');
+
 
 Auth::routes(['verify' => true]);
 
@@ -24,10 +28,10 @@ Route::group(['middleware' => ['auth', 'verified']], function() {
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
-    Route::redirect('/', '/products')->name('root');
-    Route::get('products', 'ProductsController@index')->name('products.index');
-    Route::get('products', 'ProductsController@index')->name('products.index');
-    Route::get('products/{product}', 'ProductsController@show')->name('products.show');
+
     Route::post('products/{product}/favorite', 'ProductsController@favor')->name('products.favor');
     Route::delete('products/{product}/favorite', 'ProductsController@disfavor')->name('products.disfavor');
+    Route::get('products/favorites', 'ProductsController@favorites')->name('products.favorites');
 });
+
+Route::get('products/{product}', 'ProductsController@show')->name('products.show');
